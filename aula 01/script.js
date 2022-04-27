df7880ffe9bbfde7eb79cf664a421815b50cdbd0
joaowictorzanin <jwzanin@gmail.com>
2022-04-27 15:47:12 -0300

    aula 01

---
+++ aula 01/script.js
@@ -0,0 +1,7 @@
+var nome = "João Zanin"
+var time = "Japão Japão é o melhor time do mundo!"
+console.log('Meu Primeiro JS by: ' + nome)
+console.log(time)
+console.log(time.replace("Japão", "Brasil"))
+console.log(time.toUpperCase())
+console.log(time.toLowerCase())
